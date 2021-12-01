c450e03e33358e4d93da2e58c4d133f7faa77b45
Skvortsov <skvorzov.sv@gmail.com>
2021-12-01 21:17:52 +0200

    commit 1.0

--- src/main/java/com/skvortsov/MyStack.java
+++ src/main/java/com/skvortsov/MyStack.java
@@ -1,4 +1,40 @@
 package com.skvortsov;
 
-public class MyStack {
+public class MyStack implements Stackable {
+    private int size;
+    private int[] arr = new int[size];
+    private int top;
+
+    public MyStack(int n) {
+        this.size = n;
+        arr = new int[size];
+        this.top = -1;
+    }
+
+    public void addElement(int element) {
+        if (top == size - 1) {
+            System.out.println("You cant add element. Stack is full");
+        } else {
+            arr[++top] = element;
+        }
+    }
+
+    public int deleteElement() {
+        if (top == -1) {
+            System.out.println("You cant delete element. Stack is empty");
+        }
+        return arr[top--];
+    }
+
+    public int showTopElement() {
+        return arr[top];
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public boolean isFull() {
+        return top == size - 1;
+    }
 }
--- src/main/java/com/skvortsov/Stackable.java
+++ src/main/java/com/skvortsov/Stackable.java
@@ -1,4 +1,13 @@
 package com.skvortsov;
 
 public interface Stackable {
+    void addElement(int element);
+
+    int deleteElement();
+
+    int showTopElement();
+
+    boolean isEmpty();
+
+    boolean isFull();
 }
